3e02c5e659a9771d31b8a39751dc3c2bcea183ed
shunsaku0621 <hamagi.0621@icloud.com>
2021-05-25 20:42:47 +0900

    validatesの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :delivery_id
+    validates :price
+  end
 end
