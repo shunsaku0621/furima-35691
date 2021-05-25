2733efa54dc4a7a031b0cb9cf1f09585525691ac
shunsaku0621 <hamagi.0621@icloud.com>
2021-05-25 17:24:12 +0900

    修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth, presence: true
-
+   with_options presence: true do      
+    validates :nickname, 
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/} 
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birth, 
+   end
 
   
 end
