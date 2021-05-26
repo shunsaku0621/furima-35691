89628cf5c0a2251dd18cbb0fcd8e303bf0820769
shunsaku0621 <hamagi.0621@icloud.com>
2021-05-26 12:34:30 +0900

    active hash実装

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveHash::Base
   self.data = [
-  { id: 1, name: '--' },
+  { id: 1, name: '---' },
   { id: 2, name: 'レディース' },
   { id: 3, name: 'メンズ' },
   { id: 4, name: 'ベビー・キッズ' },
@@ -15,7 +15,7 @@ class Category < ActiveHash::Base
 
 
 include ActiveHash::Associations
-  has_many :articles
+  has_many :items
 
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,20 +3,28 @@ class Item < ApplicationRecord
     validates :title
     validates :text
     # validates :category_id
-    validates :status_id
-    validates :shipping_fee_id
-    validates :prefecture_id
-    validates :delivery_id
+    # validates :status_id
+    # validates :shipping_fee_id
+    # validates :prefecture_id
+    # validates :delivery_id
     validates :price
     validates :image
   end
   #ActiveHashのバリデーション
   validates :category_id, numericality: {other_than: 1}
+  validates :status_id, numericality: {other_than: 1}
+  validates :shipping_fee_id, numericality: {other_than: 1}
+  validates :delivery_id, numericality: {other_than: 1}
+  validates :prefecture_id, numericality: {other_than: 1}
 
   
 #ActiveHashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :delivery
+  belongs_to :prefecture
 
 
 #通常のアソシエーション
