6f149cfdc06c4013792ae2367e65b2adb60655a3
shunsaku0621 <hamagi.0621@icloud.com>
2021-05-27 11:46:31 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :not_edit, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :not_edit_delite, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -35,8 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    # @item = Item.find(params[:id])
+    @item.destroy
     redirect_to root_path
   end
 
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def not_edit
+  def not_edit_delite
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
