4a127e42069ba8db9a354747094372e47251cdcd
shunsaku0621 <hamagi.0621@icloud.com>
2021-05-27 20:43:32 +0900

    様々なバリデーション

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,21 @@
 class Order < ApplicationRecord
+  #アソシエーション
   belongs_to :user
   belongs_to :item
   has_one :destination
+
+  #バリデーション
+  with_options presence: true do
+    validates :post_num
+    validates :city
+    validates :address
+    validates :phone_num
+  end
+
+  #ActiveHashのバリデーション
+  validates :prefecture_id, numericality: { other_than: 1 }
+
+  #ActiveHashのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
