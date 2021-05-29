3c0fd16bb8249be74e37fa8e759b53a0293a746c
shunsaku0621 <hamagi.0621@icloud.com>
2021-05-29 16:47:15 +0900

    sold outの条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,13 @@ class OrdersController < ApplicationController
   def create
     # binding.pry
     @order_destination = OrderDestination.new(order_params)
-    # binding.pry
-    @order_destination.valid?
-    pay_item
-    @order_destination.save
+    if @order_destination.valid?
+      pay_item
+      @order_destination.save
+      return redirect_to root_path
+    else 
+      render 'index'
+    end
       
   end
 
@@ -25,6 +28,7 @@ class OrdersController < ApplicationController
 
 
   def pay_item
+    # binding.pry
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
     Payjp::Charge.create(
       amount:  Item.find(order_params[:item_id]).price,
