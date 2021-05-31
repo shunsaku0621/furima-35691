be65d8cdec2d6fac9e2fda2aa10f75a3f2d043c9
shunsaku0621 <hamagi.0621@icloud.com>
2021-05-31 14:48:11 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
 
   def index
     @order = Item.find(params[:item_id])
